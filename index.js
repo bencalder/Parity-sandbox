975f51b709582dbf652cf7f12ca45c19e3ca5955
Ben Calder <263295+bencalder@users.noreply.github.com>
2018-07-17 16:18:20 -0500

    Formatting

--- index.js
+++ index.js
@@ -10,105 +10,99 @@ var address;
 var em;
 
 function post(params, method, callback) {
-	request({
-    	method: 'POST',
-    	json: true,
-    	uri: 'http://localhost:8545/',
-    	body: [{
-        	params: params,
-        	method: method,
-        	id: 1,
-        	jsonrpc: '2.0'
-      	}]
-  	},
-  	function (error, response, body) {
-    	if (error) {
-      		return console.error('Call failed:', error);
-    	}
-    	callback(body);
-  	})
+  request({
+    method: 'POST',
+    json: true,
+    uri: 'http://localhost:8545/',
+    body: [{
+      params: params,
+      method: method,
+      id: 1,
+      jsonrpc: '2.0'
+    }]
+  },
+  function (error, response, body) {
+    if (error) {
+      return console.error('Call failed:', error);
+    }
+    callback(body);
+  })
 }
 
 function generateSecretPhrase() {
+  post([], 'parity_generateSecretPhrase', function(body) {
+    var phrase = body[0]['result'];
+    console.log('Generated new mnemonic: ' + phrase);
+    var wallet = ethKey.phraseToWallet(phrase);
 
-	post([], 'parity_generateSecretPhrase', function(body) {
-		var phrase = body[0]['result'];
-    	console.log('Generated new mnemonic: ' + phrase);
-
-    	var wallet = ethKey.phraseToWallet(phrase);
-
-    	wallet.then(function(result) {
-    		console.log('Generated new wallet: ' + JSON.stringify(result));
-    		address = result['address'];
-  			publicKey = result['public'];
-  			newAccountFromPhrase(phrase, pw, msg);
-		}, function(err) {
-  			console.log('Error: ' + err);
-		});
-	});
+    wallet.then(function(result) {
+      console.log('Generated new wallet: ' + JSON.stringify(result));
+      address = result['address'];
+      publicKey = result['public'];
+      newAccountFromPhrase(phrase, pw, msg);
+    }, function(err) {
+      console.log('Error: ' + err);
+    });
+  });
 }
 
 function newAccountFromPhrase(phrase) {
-
-	post([phrase, pw], 'parity_newAccountFromPhrase', function(body) {
-		console.log('Added new account: ' + body[0]['result']);
-    	encrypt();
-	});
+  post([phrase, pw], 'parity_newAccountFromPhrase', function(body) {
+    console.log('Added new account: ' + body[0]['result']);
+    encrypt();
+  });
 }
 
 function encrypt() {
-	console.log('Encrypting message: ' + msg);
+  console.log('Encrypting message: ' + msg);
 
-	post([publicKey, '0x' + a2hex(msg)], 'parity_encryptMessage', function(body) {
-		em = body[0]['result'];
-    	console.log('Encrypted message: ' + em);
-    	unlockAccount()
-	});
+  post([publicKey, '0x' + a2hex(msg)], 'parity_encryptMessage', function(body) {
+    em = body[0]['result'];
+    console.log('Encrypted message: ' + em);
+    unlockAccount()
+  });
 }
 
 function unlockAccount() {
-
-	post([address, pw, null], 'personal_unlockAccount', function(body) {
-		console.log('Unlocked account? ' + body[0]['result']);
-    	decrypt();
-	});
+  post([address, pw, null], 'personal_unlockAccount', function(body) {
+    console.log('Unlocked account? ' + body[0]['result']);
+    decrypt();
+  });
 }
 
 function decrypt() {
-
-	post([address, em], 'parity_decryptMessage', function(body) {
-		var decryptedMsg = body[0]['result'];
-    	console.log('Decrypted message: ' + hex2a(decryptedMsg));
-	});
+  post([address, em], 'parity_decryptMessage', function(body) {
+    var decryptedMsg = body[0]['result'];
+    console.log('Decrypted message: ' + hex2a(decryptedMsg));
+  });
 }
 
 function a2hex(str) {
-    var arr = [];
-	for (var n = 0; n < str.length; n ++) {
-		var hex = Number(str.charCodeAt(n)).toString(16);
-		arr.push(hex);
-	}
-	return arr.join('');
+  var arr = [];
+  for (var n = 0; n < str.length; n ++) {
+    var hex = Number(str.charCodeAt(n)).toString(16);
+    arr.push(hex);
+  }
+  return arr.join('');
 }
 
 function hex2a(hexx) {
-    var hex = hexx.toString();
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
+  var hex = hexx.toString();
+  var str = '';
+  for (var i = 0; i < hex.length; i += 2)
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  return str;
 }
 
 function launch(password, message) {
+  pw = password;
+  msg = message;
 
-	pw = password;
-	msg = message;
-
-	generateSecretPhrase();
+  generateSecretPhrase();
 }
 
 module.exports = {
-	launch: launch
+  launch: launch
 }
 
 require('make-runnable');
